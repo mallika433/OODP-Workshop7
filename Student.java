d410f16d4e1aa23fc44246c9e95eabf29e723127
“mallika433” <np03cs4s250046@heraldcollege.edu.np>
2026-01-01 17:06:48 +0545

    Added Student class

---
+++ Student.java
@@ -0,0 +1,12 @@
+package week7;
+
+public class Student {
+    private String name;
+    private int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
